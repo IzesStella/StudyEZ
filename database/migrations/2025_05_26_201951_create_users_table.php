617ef8b4f94c4480ad5dc3a0f854f7002443167b
Lucas <lucasamarosouza1@gmail.com>
2025-07-14 12:51:16 -0300

    correção de bugs

--- database/migrations/2025_05_26_201951_create_users_table.php
+++ database/migrations/2025_05_26_201951_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->boolean('role')->default(true);
+            $table->string('role')->default('student');
             $table->timestamps();
         });
     }
